67c7485205b14fc63fa181ece848c85a02e731a7
Paweł Szapiel <pawel.szapiel@plum.pl>
2022-08-05 15:29:50 +0200

    Add project files.

---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    internal class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
